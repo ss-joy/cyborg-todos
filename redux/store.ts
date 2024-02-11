3cd965412144b1f79136fc6edf76f43c270d248e
Md. Sakil Sazzad Joy <89600551+ss-joy@users.noreply.github.com>
2024-02-11 08:41:51 -0800

    chore: turn off type checking

--- redux/store.ts
+++ redux/store.ts
@@ -2,4 +2,7 @@ import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { rootReducer } from "./rootReducer";
 
+//TODO:
+//add proper type checking
+//@ts-ignore
 export const store = createStore(rootReducer, composeWithDevTools());
